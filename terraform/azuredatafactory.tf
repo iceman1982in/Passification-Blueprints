960410d7096f107ec395f82c90ebc21cf2e35d4c
Samuel Vaz <s.vaz@avanade.com>
2023-01-09 20:55:33 -0500

    removing adf runtime

--- terraform/azuredatafactory.tf
+++ terraform/azuredatafactory.tf
@@ -13,23 +13,23 @@ module "data_factory" {
 
 
 
-module "datafactory_runtime" {
-  source = "../modules/data/datafactory/azuredatafactoryruntime"
-  for_each = {
-    for key, value in try(var.datafactoryruntime, {}) : key => value    
-  }
-    depends_on = [
-        module.data_factory
-     ]
-  name            =  each.value.name
-  data_factory_id =  module.data_factory[each.value.data_factory_id].id
-  location        =  local.resource_groups[each.value.resource_group_key].location
-  node_size = each.value.node_size 
-  max_parallel_executions_per_node = try(each.value.max_parallel_executions_per_node,1)
-  edition = each.value.edition
-  network_resource_group  =  try(each.value.network_resource_group, null)
-  virtual_network_name    = try(each.value.virtual_network_name, null)
-  subnet_name             = try(each.value.subnet_name, null)
-  blob_container_uri = try("${data.azurerm_storage_account.adfstr[count.index].primary_blob_endpoint}/${azurerm_storage_container.adfctr[count.index].name}", null)
-  sas_token          = try(data.azurerm_storage_account_blob_container_sas.sasctr[count.index].sas, null)
-}
+# module "datafactory_runtime" {
+#   source = "../modules/data/datafactory/azuredatafactoryruntime"
+#   for_each = {
+#     for key, value in try(var.datafactoryruntime, {}) : key => value    
+#   }
+#     depends_on = [
+#         module.data_factory
+#      ]
+#   name            =  each.value.name
+#   data_factory_id =  module.data_factory[each.value.data_factory_id].id
+#   location        =  local.resource_groups[each.value.resource_group_key].location
+#   node_size = each.value.node_size 
+#   max_parallel_executions_per_node = try(each.value.max_parallel_executions_per_node,1)
+#   edition = each.value.edition
+#   network_resource_group  =  try(each.value.network_resource_group, null)
+#   virtual_network_name    = try(each.value.virtual_network_name, null)
+#   subnet_name             = try(each.value.subnet_name, null)
+#   blob_container_uri = data.azurerm_storage_account.adfstr && data.azurerm_storage_container ? "${data.azurerm_storage_account.adfstr[count.index].primary_blob_endpoint}/${azurerm_storage_container.adfctr[count.index].name}" : null)
+#   sas_token          = try(data.azurerm_storage_account_blob_container_sas.sasctr[count.index].sas, null)
+# }
